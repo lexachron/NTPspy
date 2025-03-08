99a92cfedf31fbd6ef35eac4b182ee32c8a83ff1
lexachron <61265902+lexachron@users.noreply.github.com>
2025-03-07 17:23:20 -0800

    boundary checking

--- test_ntpdatagram.py
+++ test_ntpdatagram.py
@@ -30,6 +30,10 @@ class TestNTPdatagram(unittest.TestCase):
                     actual = getattr(ntp_packet, field)
                     expected = ntp_fields[field]
                     self.assertEqual(actual, expected, f"row {row_num}, mismatch field {field}")
+        for i in range(NTPdatagram._SIZE):
+            with self.subTest(length=i):
+                with self.assertRaises(ValueError, msg=f"invalid datagram size allowed: {i}"):
+                    NTPdatagram.from_bytes(b"\x00" * i)
 
     def test_to_bytes(self):
         """NTPdatagram -> to_bytes() -> bytestring"""
@@ -41,6 +45,29 @@ class TestNTPdatagram(unittest.TestCase):
                 bitmask = bytes(a ^ b for a, b in zip(actual, expected))
                 bitmask_split = ' '.join([bitmask[i:i+4].hex() for i in range(0, len(bitmask), 4)])
                 self.assertEqual(actual, expected, f"mismatch in bits: {bitmask_split}")
+
+    def test_boundary_values(self):
+        """test range boundary on each field"""
+        for field, (min_val, max_val) in NTPdatagram._RANGES.items():
+            with self.subTest(field=field, value=min_val):
+                kwargs = {field: min_val}
+                ntp_packet = NTPdatagram(**kwargs)
+                self.assertEqual(getattr(ntp_packet, field), min_val, f"{field} min value")
+
+            with self.subTest(field=field, value=max_val):
+                kwargs = {field: max_val}
+                ntp_packet = NTPdatagram(**kwargs)
+                self.assertEqual(getattr(ntp_packet, field), max_val, f"{field} max value")
+
+            with self.subTest(field=field, value=min_val - 1):
+                kwargs = {field: min_val - 1}
+                with self.assertRaises(ValueError, msg=f"{field} allowed below mininum"):
+                    NTPdatagram(**kwargs)
+
+            with self.subTest(field=field, value=max_val + 1):
+                kwargs = {field: max_val + 1}
+                with self.assertRaises(ValueError, msg=f"{field} allowed above maximum"):
+                    NTPdatagram(**kwargs)
 
 if __name__ == "__main__":
     unittest.main()
